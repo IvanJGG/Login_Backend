5520370fac32872c0d607aa4aabf4b0fa833e5b0
Memo Cachú <gcachu.o@gmail.com>
2024-09-13 13:38:32 -0600

    Refactor

---
+++ Login/Models/UserRegisterRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Login.Models
+{
+    public class UserRegisterRequest
+    {
+        public string? Username_register { get; set; }
+        public string? Password_register { get; set; }
+    }
+}
---
+++ Login/Models/Usuario.cs
@@ -0,0 +1,8 @@
+﻿namespace Login.Models
+{
+    public class Usuario
+    {
+        public string? NombreUsuario { get; set; }
+        public string? Contraseña { get; set; }
+    }
+}
